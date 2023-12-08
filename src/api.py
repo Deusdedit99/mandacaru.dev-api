2788fe2962bafa0e2d60f3bb311f380f2315d43e
Deusdedit99 <64427453+Deusdedit99@users.noreply.github.com>
2023-12-08 15:02:21 -0300

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -6,7 +7,7 @@ model = joblib.load('modelo_treinado.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
 
 app = Flask(__name__)
-
+CORS(app) 
 @app.route('/', methods=['POST'])
 def classificar():
     # Obter os dados da requisição
